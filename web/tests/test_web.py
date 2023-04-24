512f9dee64f455b748b314e9419344fb0c066c78
Ron Southwick <ronlsouthwick@gmail.com>
2023-04-24 11:23:01 -0400

    some basic tests to get started

--- web/tests/test_web.py
+++ web/tests/test_web.py
@@ -1,6 +1,21 @@
 from .conftest import client
 
 
-def test_request_example(client):
+def test_root_path(client):
     response = client.get("/")
-    assert b"<h2>Hello, World!</h2>" in response.data
+    assert response.status_code == 200
+
+
+def test_c4_path(client):
+    response = client.get("/c4")
+    assert response.status_code == 200
+
+
+def test_traffic_to_unknown_path_redirected(client):
+    response = client.get("/gibberish")
+    assert response.status_code == 302
+
+
+def test_traffic_to_unknown_path_redirected_to_root_404_page(client):
+    response = client.get("/gibberish")
+    assert response.location == "https://www.ronsouthwick.com/404.html"
